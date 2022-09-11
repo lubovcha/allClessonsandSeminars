cf522f6bb295f10231a19ba6555849fa4793c83a
lubovcha <chalovaljubov@mail.ru>
2022-09-11 23:20:15 +0300

    0

--- Lesson_3_C#/For/Program.cs
+++ Lesson_3_C#/For/Program.cs
@@ -1,4 +1,5 @@
-﻿string Method4(int count, string text)
+﻿/*
+string Method4(int count, string text)
 {
 string result = String.Empty;
 for(int i = 0; i < count; i++)
@@ -8,4 +9,15 @@ for(int i = 0; i < count; i++)
 return result;
 }
 string res = Method4 (10, "z");
-Console.WriteLine(res);
+Console.WriteLine(res); 
+*/
+
+// Цикл в цикле. Задача - вывод таблицы умножения на экран
+for (int i = 2; i <= 10; i++) // таблица умножения начинается с 2, i = внешний счетчик
+{
+   for (int j = 2; j <= 10; j++) // j = внутренний счетчик
+   {
+    Console.WriteLine($"{i} х {j} = {i * j}");
+   }
+   System.Console.WriteLine();
+}
