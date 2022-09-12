e1e284a6593e040a24828ef76a835a709e0ff59a
lubovcha <chalovaljubov@mail.ru>
2022-09-13 01:09:40 +0300

    w

---
+++ Seminar_4C#/Number_amount/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+456 -> 3
+
+78 -> 2
+
+89126 -> 5
+*/
+
+Console.Write("Введи число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int CountDigits (int number){ // называем CountDigits, имя метода мы называем сами, как хотим
+/*
+ int counter = 0; // переменная-счетчик
+while (number > 0) //пока кол-во чисел больше 0
+{
+    number /= 10; // будем делить на 10, тоже самое, что и number = number / 10
+    counter++;
+}
+return counter;
+*/
+int i = 0;
+for (i = 0; number > 0; i++)
+{
+number /= 10;
+}
+return i;
+}
+System.Console.WriteLine(CountDigits(n));
---
+++ Seminar_4C#/Sum1-A/Program.cs
@@ -0,0 +1,19 @@
+﻿/* 
+Напишите программу, которая принимает на вход число (А) и выдает сумму чисел от 1 до А
+7 -> 28
+4 -> 10
+8 -> 36
+*/
+
+System.Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int GetSum(int limit){ // гетсум = нужно вернуть сумму
+int sum = 0; 
+for (int i = 1; i <= limit; i++)
+{
+sum += i; //// sum = +i 
+} 
+return sum;
+}
+System.Console.WriteLine($"Сумма чисел от 1 до {n} -> {GetSum(n)}");
