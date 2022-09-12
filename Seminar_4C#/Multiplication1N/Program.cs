d755be1e34de6e93515fb6ed1bb9a4e942ac51e3
lubovcha <chalovaljubov@mail.ru>
2022-09-13 01:56:06 +0300

    1

--- Seminar_4C#/Multiplication1N/Program.cs
+++ Seminar_4C#/Multiplication1N/Program.cs
@@ -9,7 +9,7 @@ System.Console.Write("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
 int GetMultiply(int n){
-int result = 1;
+int result = 1; // переменная, которая будет считать кол-во попыток (счетчик)
 for (int i = 2; i <= n; i++) //что бы резузьтат умножался сразу на 2, а не 1*1=1
 {
 result *= i;
