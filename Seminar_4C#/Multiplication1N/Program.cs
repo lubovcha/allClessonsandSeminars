6c56700857ce702291081a49f3886c36d35cbfd8
lubovcha <chalovaljubov@mail.ru>
2022-09-13 01:55:14 +0300

    1

---
+++ Seminar_4C#/Multiplication1N/Program.cs
@@ -0,0 +1,19 @@
+﻿/* 
+Напишите программу, которая принимает на вход число N и выдаёт произведение (умножение) чисел от 1 до N.
+
+4  -> 24
+5  -> 120
+*/
+
+System.Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int GetMultiply(int n){
+int result = 1;
+for (int i = 2; i <= n; i++) //что бы резузьтат умножался сразу на 2, а не 1*1=1
+{
+result *= i;
+}
+return result;
+}
+System.Console.WriteLine($"Факториал числа {num} равен {GetMultiply(num)}");
