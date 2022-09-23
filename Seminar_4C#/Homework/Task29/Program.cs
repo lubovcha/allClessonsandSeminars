66815c0e35591ad471eb45ba3372aed27f809e23
lubovcha <chalovaljubov@mail.ru>
2022-09-23 23:00:02 +0300

    1

--- Seminar_4C#/Homework/Task29/Program.cs
+++ Seminar_4C#/Homework/Task29/Program.cs
@@ -3,5 +3,31 @@
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 6, 1, 33 -> [6, 1, 33]
 */
+int[] GetArray()
+{
+int[] result = new int[8];
+for (int i = 0; i < result.Length; i++)
+{
+result[i] = new Random().Next(100);
+}
 
-// ERROR 
+return result;
+}
+
+void PrintArray(int[] arr)
+{
+int i = 0;
+Console.Write("[");
+while (i < arr.Length)
+{
+Console.Write(arr[i]);
+i++;
+if (i < arr.Length)
+{
+ Console.Write(", ");
+}
+}
+Console.Write("]");
+}
+
+PrintArray(GetArray());
