3a1fe0a8e0bb589baf57d9d828f7d86c95cb6785
lubovcha <chalovaljubov@mail.ru>
2022-10-16 00:19:47 +0300

    шаг 1 - написана программа по созданию массива

---
+++ Final/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] GetArray()
+{
+int[] result = new int[8];
+for (int i = 0; i < result.Length; i++)
+{
+result[i] = new Random().Next(100);
+}
+
+return result;
+}
+
+void PrintArray(int[] arr)
+{
+int i = 0;
+Console.Write("[");
+while (i < arr.Length)
+{
+Console.Write(arr[i]);
+i++;
+if (i < arr.Length)
+{
+ Console.Write(", ");
+}
+}
+Console.Write("]");
+}
+
+PrintArray(GetArray());
